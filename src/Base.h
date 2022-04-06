c6e340808d9531253adaa93501496b05a23cf3d9
Key Cortés <89230619+AlphaK03@users.noreply.github.com>
2022-04-06 01:08:18 -0600

    Creación clase Base

---
+++ src/Base.h
@@ -0,0 +1,14 @@
+//
+// Created by Key Cortés on 6/4/2022.
+//
+
+#ifndef WRAPPERDESIGNPATTERN_BASE_H
+#define WRAPPERDESIGNPATTERN_BASE_H
+
+
+class Base {
+
+};
+
+
+#endif //WRAPPERDESIGNPATTERN_BASE_H
---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Key Cortés on 6/4/2022.
+//
+#include "iostream"
+
+using namespace std;
+int main(){
+
+
+    return 0;
+}
